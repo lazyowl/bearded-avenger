83980fb62493a193a2684792871eb6304f0f64c2
lazyowl <lazyowl@users.noreply.github.com>
2015-02-23 19:35:58 -0500

    moving towards scrolling.

--- smatrix.c
+++ smatrix.c
@@ -2,18 +2,20 @@
 #define MAX	20
 
 void render(SMatrix *smtx) {
-	int i = 0, screen_lines_occupied;
-	int text_line_number = 0;
+	int i = 0, len;
+	int text_line_number = 0, num_screen_lines;
 	int starting_screen_line[MAX_LINES];
 	clear();
+
 	Line *iter = smtx->tmtx->head;
-	for(; i < smtx->y_offset; i++)
-		iter = iter->next;
-	i = 0;
 	while(iter) {
-		screen_lines_occupied = render_line(i, iter->arr);
 		starting_screen_line[text_line_number] = i;
-		i += screen_lines_occupied;
+		len = strlen(iter->arr);
+		if(len == 0) {
+			i++;
+		} else {
+			i += len / COLS + ((len % COLS == 0) ? 0 : 1);
+		}
 		text_line_number++;
 		iter = iter->next;
 	}
@@ -22,9 +24,24 @@ void render(SMatrix *smtx) {
 	smtx->screen_cursor_row += smtx->tmtx->logical_cursor_col / COLS;
 	smtx->screen_cursor_col = smtx->tmtx->logical_cursor_col % COLS;
 
+	num_screen_lines = starting_screen_line[smtx->tmtx->logical_cursor_row] / COLS + (starting_screen_line[smtx->tmtx->logical_cursor_row] % COLS > 0);
+
+	if(smtx->screen_cursor_row - smtx->y_offset >= LINES) {
+		smtx->y_offset += num_screen_lines;
+	}
+
+	iter = smtx->tmtx->head;
+	for(i = 0; i < smtx->y_offset; i++) {
+		iter = iter->next;
+	}
+	i = 0;
+	while(iter) {
+		i += render_line(i, iter->arr);
+		iter = iter->next;
+	}
 	// the cursor needs to be positioned relative to the screen, not the actual text
 	// in other words, we need to map the line number in the text (cursor_line_int) to a screen line number
-	move(smtx->screen_cursor_row - smtx->y_offset, smtx->screen_cursor_col);
+	move(smtx->screen_cursor_row, smtx->screen_cursor_col);
 	refresh();
 }
 
