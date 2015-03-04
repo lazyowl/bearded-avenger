3aeca1dfbe77e39d218bb93788275c2a694296ce
lazyowl <lazyowl@users.noreply.github.com>
2015-03-03 23:36:59 -0500

    Modified code to differentiate and use appropriately screen lines and text lines for the purposes of calculating Y offsets, cursor positions, text lines to be displayed, etc.

--- smatrix.c
+++ smatrix.c
@@ -1,49 +1,66 @@
 #include "smatrix.h"
-#define MAX	20
 
+#define MAX	20
+#define CEIL(a, b) (((a) / (b)) + (((a) % (b)) > 0 ? 1 : 0))
+
+// TODO probably a more efficient way of doing this
 void render(SMatrix *smtx, TMatrix *tmtx) {
 	int i = 0, len;
 	int text_line_number = 0, num_screen_lines;
-	int starting_screen_line[MAX_LINES];
+	Line *top;
 	clear();
 
+	// based on the number of screen lines each text line takes, find the position of the cursor
 	Line *iter = tmtx->head;
 	while(iter) {
-		starting_screen_line[text_line_number] = i;
-		len = strlen(iter->arr);
-		if(len == 0) {
-			i++;
-		} else {
-			i += len / COLS + ((len % COLS == 0) ? 0 : 1);
+		if(text_line_number == tmtx->logical_cursor_row) {
+			smtx->screen_cursor_row = i + tmtx->logical_cursor_col / COLS;
 		}
+		len = strlen(iter->arr);
+		i += (len == 0) ? 1 : CEIL(len, COLS);
 		text_line_number++;
 		iter = iter->next;
 	}
-
-	smtx->screen_cursor_row = starting_screen_line[tmtx->logical_cursor_row];
-	smtx->screen_cursor_row += tmtx->logical_cursor_col / COLS;
 	smtx->screen_cursor_col = tmtx->logical_cursor_col % COLS;
 
-	num_screen_lines = starting_screen_line[tmtx->logical_cursor_row] / COLS + (starting_screen_line[tmtx->logical_cursor_row] % COLS > 0);
+	// increment the y_offset by the number of screen lines of the topmost line currently on the screen
+	// note that we must increment i by the number of screen lines each text line occupies (not simply by 1 each time)
+	top = tmtx->logical_cursor_line;
+	i = 0;
+	while(top->prev && i < LINES) {
+		top = top->prev;
+		len = strlen(top->arr);
+		i += (len == 0) ? 1 : CEIL(len, COLS);
+	}
+	len = strlen(top->arr);
+	num_screen_lines = (len == 0) ? 1 : CEIL(len, COLS);
 
+	// set y_offset as required
 	if(smtx->screen_cursor_row - smtx->y_offset >= LINES) {
 		smtx->y_offset += num_screen_lines;
 	}
 	if(smtx->screen_cursor_row < smtx->y_offset) {
-		smtx->y_offset--;
+		smtx->y_offset -= num_screen_lines;
+		if(smtx->y_offset < 0) smtx->y_offset = 0;
 	}
 
+	// go to the actual text line that is going to be rendered on the screen
 	iter = tmtx->head;
-	for(i = 0; i < smtx->y_offset; i++) {
+	i = 0;
+	while(i < smtx->y_offset && iter) {
+		len = strlen(iter->arr);
+		i += (len == 0) ? 1 : CEIL(len, COLS);
 		iter = iter->next;
 	}
+
+	// start rendering
 	i = 0;
 	while(iter) {
 		i += render_line(i, iter->arr);
 		iter = iter->next;
 	}
-	// the cursor needs to be positioned relative to the screen, not the actual text
-	// in other words, we need to map the line number in the text (cursor_line_int) to a screen line number
+
+	// move the cursor to the position calculated offset by the y_offset
 	move(smtx->screen_cursor_row - smtx->y_offset, smtx->screen_cursor_col);
 	refresh();
 }
